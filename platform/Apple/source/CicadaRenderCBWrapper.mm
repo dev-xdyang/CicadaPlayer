3a0226d8a66415837b2a693b4e017d9dca497d5b
dev-xdyang <ouyangzhengxuan@gmail.com>
2022-05-04 18:24:31 +0800

    整理代码

--- platform/Apple/source/CicadaRenderCBWrapper.mm
+++ platform/Apple/source/CicadaRenderCBWrapper.mm
@@ -18,20 +18,25 @@ bool CicadaRenderCBWrapper::OnRenderFrame(void *userData, IAFFrame *frame)
 
         if (frame->getType() == IAFFrame::FrameTypeVideo) {
             switch (frame->getInfo().video.format) {
-                case AF_PIX_FMT_APPLE_PIXEL_BUFFER: {
-                    auto *ppBFrame = dynamic_cast<PBAFFrame *>(frame);
-                    CVPixelBufferRef pixelBuffer = ppBFrame->getPixelBuffer();
+//                case AF_PIX_FMT_APPLE_PIXEL_BUFFER: { // 魔改 直接替换原CVPixelBufferRef
+//                    auto *ppBFrame = dynamic_cast<PBAFFrame *>(frame);
+//                    CVPixelBufferRef pixelBuffer = ppBFrame->getPixelBuffer();
 //                    if ([delegate respondsToSelector:@selector(applyVideoPixelBuffer:pts:)]) {
 //                        CVPixelBufferRef newPixelBuffer = [delegate applyVideoPixelBuffer:pixelBuffer pts:frame->getInfo().pts];
 //                        ppBFrame->setPixelBuffer(newPixelBuffer);
 //                    }
-//                    return false;
+//                    return false
+//                    break;
+//                }
+                case AF_PIX_FMT_APPLE_PIXEL_BUFFER: {
+                    auto *ppBFrame = dynamic_cast<PBAFFrame *>(frame);
+                    CVPixelBufferRef pixelBuffer = ppBFrame->getPixelBuffer();
                     if ([delegate respondsToSelector:@selector(onVideoPixelBuffer:pts:)]) {
                         return [delegate onVideoPixelBuffer:pixelBuffer pts:frame->getInfo().pts];
                     }
                     break;
                 }
-                case AF_PIX_FMT_YUV420P:
+                case AF_PIX_FMT_YUV420P: // 魔改 将 YUV420P转成RGB再返回
                     if ([delegate respondsToSelector:@selector(onRGBVideoPixelBuffer:pts:)]) {
                         int imgWidth = frame->getInfo().video.width;
                         int imgHeight = frame->getInfo().video.height;
